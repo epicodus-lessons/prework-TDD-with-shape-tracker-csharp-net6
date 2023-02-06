f423d0718b2bcfe249672782a7880972b5756b16
Brooke Kullberg <brookekullberg@gmail.com>
2023-02-06 11:49:12 -0800

    add MSTest Setup

---
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    // Tests will go here!
+  }
+}
