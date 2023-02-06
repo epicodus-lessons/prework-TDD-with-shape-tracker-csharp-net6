ddcee34ec9c1a9b51e77641cbc19446285cca6c3
Brooke Kullberg <brookekullberg@gmail.com>
2023-02-06 14:56:22 -0800

    add Side2 get functionality + passing test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -9,7 +9,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle(2);
+      Triangle newTriangle = new Triangle(2, 3);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
@@ -18,7 +18,7 @@ namespace ShapeTracker.Tests
     {
       // Arrange
       int length1 = 3;
-      Triangle newTriangle = new Triangle(length1);
+      Triangle newTriangle = new Triangle(length1, 2);
       // Act
       int result = newTriangle.Side1;
       // Assert
@@ -29,12 +29,24 @@ namespace ShapeTracker.Tests
     public void SetSide1_SetsValueOfSide1_Void()
     {
       // Arrange
-      Triangle newTriangle = new Triangle(3);
+      Triangle newTriangle = new Triangle(3,3);
       int newLength1 = 44;
       // Act
       newTriangle.Side1 = newLength1;
       // Assert
       Assert.AreEqual(newLength1, newTriangle.Side1);
+    }
+
+    [TestMethod]
+    public void GetSide2_ReturnsSide2_Int()
+    {
+      // Arrange
+      int length2 = 3;
+      Triangle newTriangle = new Triangle(2, length2);
+      // Act
+      int result = newTriangle.Side2;
+      // Assert
+      Assert.AreEqual(length2, result);
     }
   }
 }
--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -6,13 +6,14 @@ namespace ShapeTracker.Models
     public int Side1
     {
       get { return _side1; }
-      
       set { _side1 = value; }
     }
+    public int Side2 { get; }
 
-    public Triangle(int length1)
+    public Triangle(int length1, int length2)
     {
       _side1 = length1;
+      Side2 = length2;
     }
   }
 }
