6514befb04ed416053f0ea55d288dfa832515c93
Brooke Kullberg <brookekullberg@gmail.com>
2023-02-06 20:15:00 -0800

    add _instances getall functionality, passing test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShapeTracker.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ShapeTracker.Tests
 {
   [TestClass]
-  public class TriangleTests
+  public class TriangleTests : IDisposable
   {
+    public void Dispose()
+    {
+      Triangle.ClearAll();
+    }
+
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
@@ -127,6 +134,20 @@ namespace ShapeTracker.Tests
       string triType = equiTri.CheckType();
       // Assert
       Assert.AreEqual("equilateral triangle", triType);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllTriangleInstances_List()
+    {
+      // Arrange
+      Triangle tri1 = new Triangle(2, 2, 9);
+      Triangle tri2 = new Triangle(21, 3, 9);
+      Triangle tri3 = new Triangle(1, 3, 9);
+      List<Triangle> expected = new List<Triangle> { tri1, tri2, tri3 };
+      // Act
+      List<Triangle> actualResult = Triangle.GetAll();
+      // Assert
+      CollectionAssert.AreEqual(expected, actualResult);
     }
   }
 }
--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ShapeTracker.Models
 {
   public class Triangle
@@ -10,12 +12,14 @@ namespace ShapeTracker.Models
     }
     public int Side2 { get; set; }
     private int _side3;
+    private static List<Triangle> _instances = new List<Triangle> {};
 
     public Triangle(int length1, int length2, int length3)
     {
       _side1 = length1;
       Side2 = length2;
       _side3 = length3;
+      _instances.Add(this);
     }
 
     public int GetSide3()
@@ -48,5 +52,16 @@ namespace ShapeTracker.Models
       }
     }
 
+    public static List<Triangle> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
   }
 }
+
